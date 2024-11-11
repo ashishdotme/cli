96fd34c2c4d0d74e757bf2154143f091a87ff470
Ashish Patel <ashishsushilpatel@gmail.com>
2024-11-11 21:14:02 +0000

    chore: update install script

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 INSTALL_DIR="/usr/local/bin"
 EXECUTABLE_NAME="ap"
 COMPILED_FILE=$(mktemp)
-curl -o "$COMPILED_FILE" https://github.com/ashishdotme/cli/blob/main/dist/ap.esm.js
+curl -o "$COMPILED_FILE" https://raw.githubusercontent.com/ashishdotme/cli/refs/heads/main/dist/ap.esm.js
 sudo mkdir -p "$INSTALL_DIR"
 sudo mv "$COMPILED_FILE" "$INSTALL_DIR/$EXECUTABLE_NAME.js"
 WRAPPER="$INSTALL_DIR/$EXECUTABLE_NAME"
 sudo tee "$WRAPPER" > /dev/null <<EOL
 #!/bin/bash
-deno run --allow-read --allow-write "$INSTALL_DIR/$EXECUTABLE_NAME.js" "\$@"
+deno run -A "$INSTALL_DIR/$EXECUTABLE_NAME.js" "\$@"
 EOL
 
 sudo chmod +x "$WRAPPER"
